2df472871605681c790c6428cd47de8ac26d6fcf
Glagan <Glagan@users.noreply.github.com>
2022-09-18 02:20:04 +0200

    Update package

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,5 @@
-// Reexport your entry components here
+import Minimark from './Minimark.svelte';
+import textToNode, { type Node } from './Adapter';
+import { tags, addTag, type TagDescription } from './Tags';
+
+export { Minimark as default, Minimark, textToNode, Node, tags, addTag, TagDescription };
